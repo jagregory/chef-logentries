e6f406b89b5ffc124e85238ebfba2f41fdec7941
James Gregory <james@jagregory.com>
2013-09-05 17:50:04 +1000

    If node specifies logfiles or a server, register them automatically.

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,44 @@ apt_repository 'logentries' do
 end
 
 package 'logentries'
-package 'logentries-daemon' do
+package 'logentries-daemon'
+
+service 'logentries' do
+  supports [:start, :stop, :restart, :reload]
   action :nothing
+end
+
+if node['logentries']
+  Chef::Application.fatal!("Missing logentries server_name.") unless node['logentries']['server_name']
+  Chef::Application.fatal!("Missing logentries account_key.") unless node['logentries']['account_key']
+
+  logentries do
+    account_key node['logentries']['account_key']
+    server_name node['logentries']['server_name']
+
+    action :register
+  end
+
+  log_files = node['logentries']['log_files'] || {}
+  Chef::Application.fatal!("Expected hash of name => path for logentries log_files.") unless log_files.is_a? Hash
+
+  log_files.each do |log_name,log_path|
+    logentries log_path do
+      log_name log_name
+      action :follow
+    end
+  end
+end
+
+# restart the service if it's running
+service 'logentries' do
+  action :restart
+  only_if 'service logentries status' # status returns 0 if the service is running
+end
+
+# start the service if it isn't. We do it like this because logentries
+# start borks if the service is already running.
+service 'logentries' do
+  action :start
+  not_if 'service logentries status' # status returns 0 if the service is running
 end
