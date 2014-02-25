1a12890d0a986c99819fc05c3cdcc45abeae5b83
gleclaire <garvin.leclaire@gmail.com>
2014-02-25 00:20:40 -0500

    Add amazon yum installation

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,16 @@ maintainer_email "james@jagregory.com"
 license          "MIT"
 description      "Installs and manages Logentries."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.3"
+version          "0.1.4"
 
 recipe "logentries", "Set up the apt repository and install the logentries package"
 
 depends "apt"
+depends "yum"
 
-supports "ubuntu"
+%w{ubuntu amazon}.each do |os|
+  supports os
+end
+
 provides "logentries"
 provides "service[logentries]"
