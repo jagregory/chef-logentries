4ab1a6045fe3a8df4703fa5bbc48e71ac0960c45
Azul <mail@azulinho.com>
2015-03-16 15:57:34 +0000

    fixing typo in template

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "james@jagregory.com"
 license          "MIT"
 description      "Installs and manages Logentries."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.8"
+version          "0.1.9"
 
 recipe "logentries", "Set up the apt repository and install the logentries package"
 
